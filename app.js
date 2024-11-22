d4cadf1dfcf567a16c066c942567e2f83a75f6f5
Henrique Castilho <Henriqueribeiro200254@hotmail.com>
2024-11-21 23:08:34 -0300

    inicio do projeto blog

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import express from "express"
 import { ExpressHandlebars } from "express-handlebars"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
+import admin from "./routes/admin.js"
 const app = express()
+
 
 // Configurações
 
 // Rotas
-
+    app.use('/admin', admin)
 //Outros
 const Port = 8081
 app.listen(Port, function() {
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,16 @@
 import express from "express"
 const router = express.Router()
 
+router.get('/', function(req,res){
+   res.send("Página principal do painel ADM")
+})
+
+router.get('/posts', function(req, res){
+   res.send("Página de posts")
+})
+
+router.get('/categorias', function(req, res){
+   res.send("Página de categorias")
+})
+
+export default router
