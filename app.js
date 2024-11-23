b96c9241d733d5a86711adbf92d15713c54afffa
Henrique Castilho <Henriqueribeiro200254@hotmail.com>
2024-11-23 16:14:49 -0300

    bootstrap

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // Carregando Módulos
 import express from "express"
-import { ExpressHandlebars } from "express-handlebars"
+import { engine } from "express-handlebars"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import admin from "./routes/admin.js"
+import path from "path"
+const __dirname = path.resolve()
 const app = express()
 
 
 // Configurações
+   //Body parser
+      app.use(express.urlencoded({extended: true}))
+      app.use(express.json())
+   // Handlebars
+      app.engine('handlebars', engine())
+      app.set('view engine', 'handlebars')
+      app.set('views', './views')
+   //Mongoose
 
+   // Public
+   app.use(express.static(path.join(__dirname,"public")))
 // Rotas
     app.use('/admin', admin)
 //Outros
