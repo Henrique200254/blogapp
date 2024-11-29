9ae1dc20cd6662abe435cecc702019331c2c0fae
Henrique Castilho <Henriqueribeiro200254@hotmail.com>
2024-11-28 23:46:39 -0300

    formulário de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,13 @@ router.get('/posts', function(req, res){
 })
 
 router.get('/categorias', function(req, res){
-   res.render("admin/categorias")
+   Categoria.find().lean().sort({date: "desc"}).then((categorias) => {
+      res.render("admin/categorias", {categorias: categorias})
+   }).catch((erro) => {
+      req.flash("error_msg", "Houve um erro ao registrar as categorias")
+      res.redirect("/admin")
+   })
+   
 })
 
 router.get("/categorias/add", function (req,res) {
@@ -21,15 +27,94 @@ router.get("/categorias/add", function (req,res) {
 })
 
 router.post("/categorias/nova", (req,res) => {
-   const novaCategoria = {
-      nome: req.body.nome,
-      slug: req.body.slug
+   
+   var erros = []
+
+   if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+       erros.push({texto: "nome inválido"})
+   }
+   
+   if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+       erros.push({texto: "Slug inválido"})
    }
 
-   new Categoria(novaCategoria).save().then(() => {
-      console.log("Categoria salva com sucesso!")
+   if(req.body.nome.length < 2){
+      erros.push({texto: "Nome da categoria muito pequeno"})
+   }
+   
+   if(erros.length > 0){
+       res.render("admin/addcategorias", {erros: erros})
+   } else {
+      const novaCategoria = {
+         nome: req.body.nome,
+         slug: req.body.slug
+      }
+   
+      new Categoria(novaCategoria).save().then(() => {
+         req.flash("success_msg", "Categoria criada com sucesso!")
+         res.redirect("/admin/categorias")
+      }).catch((erro) => {
+         req.flash("error_msg", "Houve um erro ao salvar a categoria!")
+         res.redirect("/admin")
+      })
+   }
+   
+})
+
+router.get("/categorias/edit/:id", (req,res) => {
+   Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+      res.render("admin/editcategorias", {categoria: categoria})
    }).catch((erro) => {
-      console.log("Erro ao salvar categoria! "+ erro)
+      req.flash("error_msg", "Esta categoria não existe")
+      res.redirect("/admin/categorias")
+   })
+   
+})
+
+router.post("/categorias/edit", (req,res) => {
+   Categoria.findOne({_id: req.body.id}).then( (categoria) => {
+
+      categoria.nome = req.body.nome
+      categoria.slug = req.body.slug
+
+      categoria.save().then(() => {
+          req.flash('success_msg', 'Categoria editada com Sucesso!')
+          res.redirect('/admin/categorias')
+      }).catch( (erro) => { // ERR: Erro ao salvar::editar
+          req.flash('error_msg', 'Houve um erro interno ao editar categoria.')
+          res.redirect("/admin/categorias")
+      })
+
+  }).catch( (erro) => { // ERR: Não encontrou a categoria
+      req.flash('error_msg', 'Houve um erro ao editar categoria.') //Mensagem de Erro
+      res.redirect("/admin/categorias")
+  })
+   
+})
+
+router.post("/categorias/deletar", (req,res) => {
+   Categoria.deleteOne({_id: req.body.id}).then(() => {
+      req.flash("success_msg", "Categoria deletada com sucesso!")
+      res.redirect("/admin/categorias")
+   }).catch((erro) => {
+      req.flash("error_msg", "Houve um erro ao deletar a categoria")
+      res.redirect("/admin/categorias")
    })
 })
+
+router.get("/postagens", (req, res) => {
+   res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req,res) => {
+   Categoria.find().lean().then((categorias) => {
+      res.render("admin/addpostagem", {categorias: categorias})
+   }).catch((erro) => {
+      req.flash("error_msg", "Houve um erro ao carregar o formulário")
+      res.redirect("/admin")
+   })
+   
+})
+
+
 export default router
